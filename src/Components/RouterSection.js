9af60f047715319acdb44e7987def79d0fdc201c
Criss Martin <nitramssirc@yahoo.com>
2020-05-12 23:11:15 -0500

    Added accordian to router section

--- src/Components/RouterSection.js
+++ src/Components/RouterSection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Accordion, Card } from "react-bootstrap";
 
 import MainContent from "./MainContent";
 import TechUsed from "./TechUsed";
@@ -7,14 +8,43 @@ import Blogs from "./Blogs";
 import Sources from "./Sources";
 
 const RouterSection = () => {
-    let { id } = useParams();
+  let { id } = useParams();
 
-    return (
+  return (
     <>
       <MainContent id={id} />
-      <TechUsed name={id} />
-      <Blogs />
-      <Sources />
+      <Accordion defaultActiveKey="0">
+        <Card>
+            <Accordion.Toggle as={Card.Header} variant="link" eventKey="1">
+              Tech Used
+            </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>
+              <TechUsed name={id} />
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+            <Accordion.Toggle as={Card.Header} variant="link" eventKey="2">
+              Blog Posts
+            </Accordion.Toggle>
+          <Accordion.Collapse eventKey="2">
+            <Card.Body>
+              <Blogs />
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+            <Accordion.Toggle as={Card.Header} variant="link" eventKey="3">
+              Source
+            </Accordion.Toggle>
+          <Accordion.Collapse eventKey="3">
+            <Card.Body>
+              <Sources />
+            </Card.Body>
+          </Accordion.Collapse> 
+        </Card>
+      </Accordion>
     </>
   );
 };
