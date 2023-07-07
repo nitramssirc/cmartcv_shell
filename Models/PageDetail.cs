2afe77804197876f2ab1f56179cb3796dc7cc8cc
Criss Martin <nitramssirc@yahoo.com>
2023-07-06 23:27:37 -0400

    Created shared components to hold the logic for swapping between page view and page details.
    Set index to use the new components

---
+++ Models/PageDetail.cs
@@ -0,0 +1,13 @@
+﻿namespace CMartCV_Shell.Models
+{
+    public class PageDetail
+    {
+        public string[] TechUsed { get; set; }
+
+        public string[] SourceLinks { get; set; }
+
+        public string[] BlogPosts { get; set; }
+
+        public string[] DiagramImages { get; set; }
+    }
+}
